6dd53f9bf31d04d3bdaed09622495001b12fb8ab
Bonnie <aalle.allen@gmail.com>
2015-08-31 16:57:39 -0400

    Checking off and deleting tasks

--- App.jsx
+++ App.jsx
@@ -1,28 +1,69 @@
 // App component - represents the whole app
 App = React.createClass({
 
-   mixins: [ReactMeteorData],
+  // This mixin makes the getMeteorData method work
+  mixins: [ReactMeteorData],
 
-
-     // Loads items from the Tasks collection and puts them on this.data.tasks
-  getMeteorData() {
+  getInitialState() {
     return {
-      tasks: Tasks.find({}).fetch()
+      hideCompleted: false
     }
   },
- 
+
+  // Loads items from the Tasks collection and puts them on this.data.tasks
+  getMeteorData() {
+    let query = {};
+
+    if (this.state.hideCompleted) {
+      // If hide completed is checked, filter tasks
+      query = {checked: {$ne: true}};
+    }
+
+    return {
+      tasks: Tasks.find(query, {sort: {createdAt: -1}}).fetch(),
+      incompleteCount: Tasks.find({checked: {$ne: true}}).count(),
+      currentUser: Meteor.user()
+    };
+  },
+
   renderTasks() {
-    return this.getTasks().map((task) => {
-      return <Task key={task._id} task={task} />;
+    // Get tasks from this.data.tasks
+    return this.data.tasks.map((task) => {
+      const currentUserId = this.data.currentUser && this.data.currentUser._id;
+      const showPrivateButton = task.owner === currentUserId;
+
+      return <Task
+        key={task._id}
+        task={task}
+        showPrivateButton={showPrivateButton} />;
     });
   },
- 
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // Find the text field via the React ref
+    var text = React.findDOMNode(this.refs.textInput).value.trim();
+
+    Meteor.call("addTask", text);
+
+    // Clear form
+    React.findDOMNode(this.refs.textInput).value = "";
+  },
+
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: ! this.state.hideCompleted
+    });
+  },
+
   render() {
     return (
       <div className="container">
         <header>
           <h1>Todo List ({this.data.incompleteCount})</h1>
-         <label className="hide-completed">
+
+          <label className="hide-completed">
             <input
               type="checkbox"
               readOnly={true}
@@ -44,7 +85,7 @@ App = React.createClass({
         </header>
 
         <ul>
-        {this.renderTasks()}
+          {this.renderTasks()}
         </ul>
       </div>
     );
